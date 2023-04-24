2577026e312ff4662b2ed1b57ced66dc95a35f27
kidaei rawia <kidaeirawia191@gmail.com>
2023-04-24 16:00:27 +0200

    mrsync

--- client.py
+++ client.py
@@ -1,5 +1,15 @@
 import os
-octets = os.read(0, 100)
-os.write(1, octets)
-os.write(
-    2, (f"{len(octets)} octets recopiés stdin->stdout\nbye\n").encode('utf-8'))
+'''
+in client.py
+il est a l'écoute sur le pipe en lecture et lit ce quelle est envoyé sur ce tube 
+switch(query[0]):
+    case makedirs :
+        appelle une fonction qui creer les dossier d' une façon récurssive
+        query[1]
+    case makefiles:
+        appelle une fonction qui creer le fichier a l'intérieur du dst et le nom du fichier = query[1]
+    case write file:
+        appelle une fonction qui ecrit query [1] à l'intrieur du fichier de destination
+        
+        
+'''
